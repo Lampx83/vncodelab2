1986cd778ab1d90cc137f09f45f7df7fd6135948
xuanlam <phamxuanlam@gmail.com>
2021-08-16 00:24:40 +0700

    Update Lab

--- src/main/java/com/vncodelab/controller/MainController.java
+++ src/main/java/com/vncodelab/controller/MainController.java
@@ -68,7 +68,7 @@ public class MainController {
             }
 
           //  Process p = Runtime.getRuntime().exec(System.getProperty("user.home") + "/go/bin/claat export " + newLab.getDocID());
-             Process p = Runtime.getRuntime().exec("/home/phamxuanlam/go/bin/claat export " + newLab.getDocID());  //For Google Cloud
+             Process p = Runtime.getRuntime().exec("/home/phamxuanlam/work/bin/claat export " + newLab.getDocID());  //For Google Cloud
             BufferedReader input = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String line = input.readLine();
             p.waitFor();
