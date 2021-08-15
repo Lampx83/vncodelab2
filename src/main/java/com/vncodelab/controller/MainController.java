d9085a7569f184843bae32775c9752123172825d
xuanlam <phamxuanlam@gmail.com>
2021-08-16 01:43:35 +0700

    Update Lab

--- src/main/java/com/vncodelab/controller/MainController.java
+++ src/main/java/com/vncodelab/controller/MainController.java
@@ -85,7 +85,8 @@ public class MainController {
              // Process p = Runtime.getRuntime().exec("/home/phamxuanlam/work/bin/claat export " + newLab.getDocID());  //For Google Cloud
 
             ProcessBuilder builder = new ProcessBuilder();
-            builder.command( "~/work/bin/claat export " + newLab.getDocID());
+            builder.command( "/home/phamxuanlam/work/bin/claat","export", newLab.getDocID());
+           // builder.command("ls");
            // builder.directory(new File(System.getProperty("user.home")));
             Process p = builder.start();
 
