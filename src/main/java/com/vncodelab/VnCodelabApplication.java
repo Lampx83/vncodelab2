f947fd6cf9d51ff6d033aee2ecb2cc16728e72ee
xuanlam <phamxuanlam@gmail.com>
2021-08-15 12:55:56 +0700

    Update Lab

--- src/main/java/com/vncodelab/VnCodelabApplication.java
+++ src/main/java/com/vncodelab/VnCodelabApplication.java
@@ -7,14 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
 @SpringBootApplication
 public class VnCodelabApplication extends SpringBootServletInitializer {
-    private static final Logger log = LoggerFactory.getLogger(VnCodelabApplication.class);
-
-
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(VnCodelabApplication.class, args);
     }
-
 
 }
