7fc3e2dcbe4190de3d22063718e2f6fd7a2e612c
xuanlam <phamxuanlam@gmail.com>
2021-08-15 19:59:55 +0700

    Update Lab

--- test.java
+++ test.java
@@ -4,9 +4,21 @@ import java.io.InputStreamReader;
 
 public class test {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Process p = Runtime.getRuntime().exec("/home/phamxuanlam/go/bin/claat export 1rz-UJcd5wQ-giAdIm81bEQoT94xuUJwTj5eik_8LDA4");
-        BufferedReader input = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-        String line = input.readLine();
-        p.waitFor();
+        try {
+            Process p = Runtime.getRuntime().exec("/home/phamxuanlam/go/bin/claat export 1rz-UJcd5wQ-giAdIm81bEQoT94xuUJwTj5eik_8LDA4");
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String line = input.readLine();
+            p.waitFor();
+            System.out.println("Done87");
+            String folderName = line.split("\t")[1];
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(folderName + "/codelab.json")));
+            String totalLine = "";
+            while ((line = br.readLine()) != null)
+                totalLine = totalLine + line;
+            System.out.println("Done93");
+        } catch (Exception ex) {
+            System.out.println("Exception");
+            ex.printStackTrace();
+        }
     }
 }
