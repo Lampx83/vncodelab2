e97469d0c06c94f4d49984162a617f9b15c355c5
xuanlam <phamxuanlam@gmail.com>
2021-09-19 10:28:21 +0700

    Update Lab

--- src/main/java/com/vncodelab/controller/FileController.java
+++ src/main/java/com/vncodelab/controller/FileController.java
@@ -77,12 +77,14 @@ public class FileController {
     public String claat() {
         try {
 
-            Process p = Runtime.getRuntime().exec("/home/phamxuanlam/go/bin/claat export 1rz-UJcd5wQ-giAdIm81bEQoT94xuUJwTj5eik_8LDA4");
+           // Process p = Runtime.getRuntime().exec("/home/phamxuanlam/go/bin/claat export 1rz-UJcd5wQ-giAdIm81bEQoT94xuUJwTj5eik_8LDA4");
+            Process p = Runtime.getRuntime().exec("java -v");
             BufferedReader input = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String line = input.readLine();
             System.out.println("Dong "+ line);
             p.waitFor();
-            return "91";
+
+            return line;
 //            System.out.println("Done87");
 //            String folderName = line.split("\t")[1];
 //            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(folderName + "/codelab.json")));
