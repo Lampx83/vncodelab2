9c4f8bbb5d9288a325bbbfcdb4cba2eab4ced68c
xuanlam <phamxuanlam@gmail.com>
2021-09-19 10:34:59 +0700

    Update Lab

--- src/main/java/com/vncodelab/controller/FileController.java
+++ src/main/java/com/vncodelab/controller/FileController.java
@@ -78,18 +78,21 @@ public class FileController {
     public String claat() {
         try {
 
-            Scanner s = new Scanner(System.in);
-            System.out.println("Nhap vao mot so");
-            int x = s.nextInt();
-            System.out.println(x);
+//            Scanner s = new Scanner(System.in);
+//            System.out.println("Nhap vao mot so");
+//            int x = s.nextInt();
+//            System.out.println(x);
             //Process p = Runtime.getRuntime().exec("/home/phamxuanlam/go/bin/claat export 1rz-UJcd5wQ-giAdIm81bEQoT94xuUJwTj5eik_8LDA4");
-        //    Process p = Runtime.getRuntime().exec("java -v");
-          //  BufferedReader input = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-          //  String line = input.readLine();
-           // System.out.println("Dong "+ line);
-          //  p.waitFor();
 
-            return ""+x;
+            Process p = Runtime.getRuntime().exec("./claat export 1rz-UJcd5wQ-giAdIm81bEQoT94xuUJwTj5eik_8LDA4");
+
+            //    Process p = Runtime.getRuntime().exec("java -v");
+            BufferedReader input = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            String line = input.readLine();
+            System.out.println("Dong "+ line);
+            p.waitFor();
+
+            return line;
 //            System.out.println("Done87");
 //            String folderName = line.split("\t")[1];
 //            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(folderName + "/codelab.json")));
